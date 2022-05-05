2ee2a32011fc4c18a85babf9dba0b395de88a865
unknown <tyagipriyanshi79@gmail.com>
2022-05-06 02:17:17 +0530

    connecting database

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+//var createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+/*var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');*/
 
-var app = express();
+const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://Curelink:Tyagi123@cluster0.wulgd.mongodb.net/Content?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3001))
+  .catch((err) => console.log(err));
+
+
+
+//app.listen(3000);
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+/*app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -36,6 +46,8 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
 module.exports = app;
+
+
